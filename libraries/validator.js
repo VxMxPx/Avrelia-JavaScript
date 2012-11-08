481d0084920b31eceb72c33042d8b40a3913a024
Marko Gajst <m@avrelia.com>
2012-11-08 11:28:31 +0000

    Validator line limit fixed.

--- libraries/validator.js
+++ libraries/validator.js
@@ -40,7 +40,7 @@ AJS.register('Library.Validator', function() {
         _validate_field: function(field, rule) {
             var type = field.attr('type');
 
-            // Required rule -------------------------------------------------------
+            // Required rule ---------------------------------------------------
             if (rule.required === true) {
                 if (type === 'checkbox') {
                     if (field.attr('checked') !== 'checked') {
@@ -56,7 +56,7 @@ AJS.register('Library.Validator', function() {
                 }
             }
 
-            // Rule length ---------------------------------------------------------
+            // Rule length -----------------------------------------------------
             if (rule.length) {
                 if (field.val().length !== rule.length) {
                     this.message.warn(rule.message);
@@ -64,7 +64,7 @@ AJS.register('Library.Validator', function() {
                 }
             }
 
-            // Minimum length ------------------------------------------------------
+            // Minimum length --------------------------------------------------
             if (rule.min_length) {
                 if (field.val().length < rule.min_length) {
                     this.message.warn(rule.message);
@@ -72,20 +72,21 @@ AJS.register('Library.Validator', function() {
                 }
             }
 
-            // Rule equals ---------------------------------------------------------
+            // Rule equals -----------------------------------------------------
             if (rule.equals) {
                 var rule_equals_val = 
-                            typeof this.$form.find(rule.equals)['val'] !== 'undefined'
-                                ? this.$form.find(rule.equals)['val']()
-                                : false;
+                    typeof this.$form.find(rule.equals)['val'] !== 'undefined'
+                        ? this.$form.find(rule.equals)['val']()
+                        : false;
 
-                if (rule_equals_val === false || field.val() !== rule_equals_val) {
+                if (rule_equals_val === false || 
+                        field.val() !== rule_equals_val) {
                     this.message.warn(rule.message);
                     this.valid = false;
                 }
             }
 
-            // Rule match ----------------------------------------------------------
+            // Rule match ------------------------------------------------------
             if (rule.is_match) {
                 if (field.val().match(rule.is_match) === null) {
                     this.message.warn(rule.message);
@@ -93,7 +94,7 @@ AJS.register('Library.Validator', function() {
                 }
             }
 
-            // Rule is_email -------------------------------------------------------
+            // Rule is_email ---------------------------------------------------
             if (rule.is_email) {
                 if (field.val().match(/^(.+@.+\..+)$/) === null) {
                     this.message.warn(rule.message);
