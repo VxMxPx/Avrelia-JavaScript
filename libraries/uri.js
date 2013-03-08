1bbf40540a22dd681a8db3ad909ecda13b7d1602
Marko Gajst <m@avrelia.com>
2013-03-08 13:11:59 +0000

    Uri fetch(refresh) segments always when need one of them.

--- libraries/uri.js
+++ libraries/uri.js
@@ -5,6 +5,16 @@ AJS.register('Library.Uri', function() {
         segments = [];
 
     /**
+     * Will extract segments from the URI and put them to the segments array
+     * @return {[type]} [description]
+     */
+    function _extract_segments() {
+        // GET SEGMENTS, trim start and ending slash
+        segments = window.location.pathname.replace(/^\/|\/$/g, '');
+        segments = segments.split('/');
+    }
+
+    /**
      * Get particular URI's segment
      * --
      * @param  {integer} index
@@ -12,7 +22,7 @@ AJS.register('Library.Uri', function() {
      * @return {string}
      */
     _public.segment = function(index) {
-
+        _extract_segments();
         if (typeof segments[index] !== 'undefined') {
             return segments[index];
         }
@@ -26,10 +36,6 @@ AJS.register('Library.Uri', function() {
     _public.current = function() {
         return window.location.pathname + (window.location.hash || '');
     };
-
-    // GET SEGMENTS, trim start and ending slash
-    segments = window.location.pathname.replace(/^\/|\/$/g, '');
-    segments = segments.split('/');
 
     return _public;
 
