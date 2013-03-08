fac0482caeb065cefd6382633ff660317e3ffd79
Marko Gajst <m@avrelia.com>
2013-03-08 13:00:17 +0000

    Better uri handling.

--- libraries/uri_hash.js
+++ libraries/uri_hash.js
@@ -5,32 +5,55 @@ AJS.register('Library.UriHash', function() {
         // it would be: [ 'events', 'list', {sort: 'desc'} ]
         segments = [];
 
-
     /**
-     * This will, at the moment just clear out empty segments.
+     * Will extract segments from URI
      * --
      * @return {void}
      */
-    function _rebuild_uri() {
+    function _extract_segments()
+    {
+        // Resolve URI
+        var temp_segments = window.location.hash.split('/');
 
-        var new_segments = [];
+        if (temp_segments.length) {
+            // We wanna keep segments only if we have #!
+            if (temp_segments[0] !== '#!' && temp_segments[0].substr(0,2) !== '#!') {
+                segments = [];
+            }
+            else {
 
-        for (var i = 0; i < segments.length; i++) {
-            if (segments[i] !== false) {
-                new_segments.push(segments[i]);
+                if (temp_segments[0].substr(0,2) === '#!') {
+                    temp_segments[0] = temp_segments[0].substr(2);
+                }
+                else {
+                    temp_segments[0] = '';
+                }
+
+                for (var i = 0; i < temp_segments.length; i++) {
+
+                    if (!temp_segments[i].length) { continue; }
+
+                    if (temp_segments[i].match(/\=/)) {
+                        var sub_segments = temp_segments[i].split('=', 2);
+                        temp_segments[i] = {
+                            key: sub_segments[0],
+                            val: sub_segments[1]
+                        };
+                    }
+
+                    segments.push(temp_segments[i]);
+                }
             }
         }
-
-        segments = new_segments;
     }
 
     /**
-     * Return an array of all currently set segments.
+     * Return an array of all currently set segments in the hash.
      * --
      * @return {void}
      */
     _public.get_segments = function() {
-
+        _extract_segments();
         return segments;
     };
 
@@ -40,17 +63,13 @@ AJS.register('Library.UriHash', function() {
      * @return {void}
      */
     _public.get_full = function() {
-
         var full_hash = '#!';
-
         for (var i = 0, l = segments.length; i < l; i++) {
-
             if (segments[i] === false) {
                 continue;
             }
 
             if (typeof segments[i].key !== 'undefined') {
-
                 full_hash = full_hash + '/' + segments[i].key + '=' + segments[i].val;
             }
             else {
@@ -72,19 +91,15 @@ AJS.register('Library.UriHash', function() {
      * @return {string}
      */
     _public.get = function(segment) {
-
         if (typeof segment === 'number') {
-
             if (typeof segments[segment] !== undefined) {
+
                 return segments[segment];
             }
         }
         else {
-
             for (var i = segments.length - 1; i >= 0; i--) {
-
                 if (typeof segments[i].key !== 'undefined') {
-
                     if (segments[i].key === segment) {
 
                         return segments[i].val;
@@ -104,13 +119,13 @@ AJS.register('Library.UriHash', function() {
      * @return {boolean}
      */
     _public.has = function(segment) {
-
         // Grab result and cast it as boolean
         return !!_public.get(segment);
     };
 
     /**
      * Change URI, either by appending or replacing segments.
+     * You need to call apply after this action.
      * --
      * @param {mixed}  segment Either integer: segment index, or string: get-key,
      *                         or boolean: false to append segment to the end of URI.
@@ -119,39 +134,28 @@ AJS.register('Library.UriHash', function() {
      * @return {object} this
      */
     _public.set = function(segment, value) {
-
         if (segment === false) {
-
             segments.push(value);
         }
         else if (typeof segment === 'number') {
-
             if (typeof segments[segment] === 'undefined') {
-
                 segments.push(value);
             }
             else {
-
                 segments[segment] = value;
             }
         }
         else {
-
             if (_public.get(segment) !== false) {
-
                 for (var i = segments.length - 1; i >= 0; i--) {
-
                     if (typeof segments[i].key !== 'undefined') {
-
                         if (segments[i].key === segment) {
-
                             segments[i].val = value;
                         }
                     }
                 }
             }
             else {
-
                 segments.push({
                     key: segment,
                     val: value
@@ -168,8 +172,8 @@ AJS.register('Library.UriHash', function() {
      * @return {object} this
      */
     _public.apply = function() {
-
         window.location.hash = _public.get_full();
+
         return _public;
     };
 
@@ -181,60 +185,35 @@ AJS.register('Library.UriHash', function() {
      * @return {object} this
      */
     _public.remove = function(segment) {
-
+        var i = 0;
         if (typeof segment === 'number') {
-
             if (typeof segments[segment] !== 'undefined') {
                 segments[segment] = false;
             }
         }
         else {
-
-            for (var i = segments.length - 1; i >= 0; i--) {
-
+            for (i = segments.length - 1; i >= 0; i--) {
                 if (typeof segments[i].key !== 'undefined') {
-
                     if (segments[i].key === segment) {
-
                         segments[i] = false;
                     }
                 }
             }
         }
 
-        // Just clean out empty spaces
-        _rebuild_uri();
+        for (i = 0; i < segments.length; i++) {
+            if (segments[i] !== false) {
+                new_segments.push(segments[i]);
+            }
+        }
+
+        segments = new_segments;
 
         return _public;
     };
 
-    // Resolve URI
-    var temp_segments = window.location.hash.split('/');
-
-    if (temp_segments.length) {
-
-        // We wanna keep segments only if we have #!
-        if (temp_segments[0] !== '#!') {
-            segments = [];
-        }
-        else {
-
-            for (var i = 1; i < temp_segments.length; i++) {
-
-                if (temp_segments[i].match(/\=/)) {
-
-                    var sub_segments = temp_segments[i].split('=', 2);
-
-                    temp_segments[i] = {
-                        key: sub_segments[0],
-                        val: sub_segments[1]
-                    };
-                }
-
-                segments.push(temp_segments[i]);
-            }
-        }
-    }
+    // Will extract segments from the URI
+    _extract_segments();
 
     return _public;
 
